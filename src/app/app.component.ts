b46112fd033681c2d850ba5cdb7978cd003f91cb
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-06-09 12:31:16 -0400

    Creates banner component and sets up test specs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Hello {{name}}</h1>
+    <app-banner></app-banner>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  {
+  name = 'Angular';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { BannerComponent } from './banner-inline.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    BannerComponent
+   ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
